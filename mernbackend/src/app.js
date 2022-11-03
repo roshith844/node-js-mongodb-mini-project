f702f31b673881dcbee2f63686aa06308a874ec0
Roshith P <roshith844@gmail.com>
2022-11-03 19:24:16 +0530

    Set Up : connect to Mongoose

--- mernbackend/src/app.js
+++ mernbackend/src/app.js
@@ -1,5 +1,11 @@
-const express = require('express')
-const app = express()
-app.get('/', (req,res)=>{
-     res.send("homepage")
-})
+const express = require("express");
+require("./db/conn.js");
+const app = express();
+const port = process.env.PORT || 3000;
+app.get("/", (req, res) => {
+  res.send("homepage");
+});
+
+app.listen(port, () => {
+  console.log("server running");
+});
