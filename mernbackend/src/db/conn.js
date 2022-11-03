5c5ab01d73248198e49ff52a6407fea34ee10070
Roshith P <roshith844@gmail.com>
2022-11-03 19:40:55 +0530

    comment out optional codes

--- mernbackend/src/db/conn.js
+++ mernbackend/src/db/conn.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 mongoose
   .connect("mongodb://localhost:27017/yt", {
     // For not getting depreciation warning from mongoose
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
     // useCreateIndex: true
   })
   .then(() => {
