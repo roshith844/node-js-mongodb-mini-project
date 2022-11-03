a887bd9a3629c88f613b173609136a2ad2d13bc4
Roshith P <roshith844@gmail.com>
2022-11-03 19:56:58 +0530

    Set Up: static files

--- mernbackend/src/app.js
+++ mernbackend/src/app.js
@@ -1,7 +1,13 @@
 const express = require("express");
 require("./db/conn.js");
+const path = require("path")
 const app = express();
 const port = process.env.PORT || 3000;
+
+const static_path = path.join(__dirname, "../public")
+// Middleware to acess public files
+app.use(express.static(static_path))
+
 app.get("/", (req, res) => {
   res.send("homepage");
 });
