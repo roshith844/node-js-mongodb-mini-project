017a1d98e84b59266c311833d8dff5b0124a1a86
Roshith P <roshith844@gmail.com>
2022-11-04 13:19:13 +0530

    Feat : Data stored in Database

--- mernbackend/src/app.js
+++ mernbackend/src/app.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const http = require("http");
 require("./db/conn.js");
-const Register = require("./models/registers");
-const { json } = require("express");
+
 const path = require("path");
 const hbs = require("hbs");
+const { default: mongoose } = require("mongoose");
 const app = express();
-
-const port = process.env.PORT || 3000;
 
 const static_path = path.join(__dirname, "../public");
 const templates_path = path.join(__dirname, "../templates/views");
@@ -25,10 +23,22 @@ hbs.registerPartials(partials_path);
 app.get("/", (req, res) => {
   res.render("register");
 });
-
+var db = mongoose.connection;
 app.post("/reg", (req, res) => {
   var email = req.body.email;
   var password = req.body.password;
+  var data = {
+    email: email,
+    password: password,
+  };
+  mongoose.connection.collection("users").insertOne(data, (err, collection) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log("data inserted");
+      res.render("index");
+    }
+  });
 });
 
 http.createServer(app).listen(3000);
