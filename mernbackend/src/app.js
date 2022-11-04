1d1762288f4e4da5cc658c5a130f8ffd8a8ec60e
Roshith P <roshith844@gmail.com>
2022-11-04 12:55:23 +0530

    Feat  : routing

--- mernbackend/src/app.js
+++ mernbackend/src/app.js
@@ -1,14 +1,20 @@
 const express = require("express");
+const http = require("http");
 require("./db/conn.js");
+const Register = require("./models/registers");
+const { json } = require("express");
 const path = require("path");
 const hbs = require("hbs");
 const app = express();
+
 const port = process.env.PORT || 3000;
 
 const static_path = path.join(__dirname, "../public");
 const templates_path = path.join(__dirname, "../templates/views");
 const partials_path = path.join(__dirname, "../templates/partials");
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 // Middleware to acess public files
 app.use(express.static(static_path));
 
@@ -17,9 +23,12 @@ app.set("views", templates_path);
 hbs.registerPartials(partials_path);
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("register");
 });
 
-app.listen(port, () => {
-  console.log("server running");
+app.post("/reg", (req, res) => {
+  var email = req.body.email;
+  var password = req.body.password;
 });
+
+http.createServer(app).listen(3000);
