6b096ad2fe7ed87ff99f60292d2d39058c0e75e5
Roshith P <roshith844@gmail.com>
2022-11-04 10:38:55 +0530

    Set Up :  views and partials

--- mernbackend/src/app.js
+++ mernbackend/src/app.js
@@ -1,15 +1,23 @@
 const express = require("express");
 require("./db/conn.js");
-const path = require("path")
+const path = require("path");
+const hbs = require("hbs");
 const app = express();
 const port = process.env.PORT || 3000;
 
-const static_path = path.join(__dirname, "../public")
+const static_path = path.join(__dirname, "../public");
+const templates_path = path.join(__dirname, "../templates/views");
+const partials_path = path.join(__dirname, "../templates/partials");
+
 // Middleware to acess public files
-app.use(express.static(static_path))
+app.use(express.static(static_path));
+
+app.set("view engine", "hbs");
+app.set("views", templates_path);
+hbs.registerPartials(partials_path);
 
 app.get("/", (req, res) => {
-  res.send("homepage");
+  res.render("index");
 });
 
 app.listen(port, () => {
