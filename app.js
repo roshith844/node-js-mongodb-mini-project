3205b5c95bb9141de4e61eb5f0627f3a7319fad6
Roshith P <roshith844@gmail.com>
2022-11-05 11:42:08 +0530

    login page system

--- app.js
+++ app.js
@@ -1,10 +1,87 @@
-const express = require("express");
+// calls required modules
 const http = require("http");
-const userRoute = require("./routes/user")
-const app = express()
+const express = require("express");
+const app = express();
+const expressLayouts = require("express-ejs-layouts");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+// calls morgan
+var morgan = require("morgan");
+/* initialises npm modules */
+// using morgan middleware
+app.use(morgan("tiny"));
 
-app.use('/', userRoute )
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// middlewares not to store cache
+app.use((req, res, next) => {
+  res.set("Cache-Control", "no-store");
+  next();
+});
+// initiates cookie and session
+app.use(cookieParser());
+app.use(
+  session({
+    resave: true,
+    saveUninitialized: true,
+    secret: "secret",
+  })
+);
 
-app.listen(3000)
+app.use(expressLayouts);
+app.set("layout", "./layouts/page.ejs");
+app.set("view engine", "ejs");
+
+//predefined email and password
+const user = {
+  userName: "pass@mail.com",
+  password: 123,
+};
+app.use(
+  session({
+    secret: "pass@mail.com",
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 },
+    resave: false,
+  })
+);
+
+//navigates to routes
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+// logs out with destroying session
+app.get("/logout", (req, res) => {
+  req.session.destroy((error) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("logout successfully");
+      res.redirect("/login");
+    }
+  });
+});
+
+app.get("/", (req, res) => {
+  if (req.session.user) {
+    res.render("home");
+  } else {
+    res.redirect("/login");
+  }
+});
+app.post("/login", (req, res) => {
+  if (req.body.email === user.userName && req.body.password == user.password) {
+    req.session.user = req.body.email;
+    console.log("Logged In");
+    res.redirect("/");
+  } else {
+    res.render("login", { layout: "./layouts/invalid.ejs" });
+  }
+});
+
+//server
+
+http.createServer(app).listen(8000, () => {
+  console.log("running");
+});
--- routes/user.js
+++ routes/user.js
@@ -2,8 +2,37 @@
 const express= require('express')
 let router = express.Router()
 
-router.get("/test", (req, res) => {
-     res.send("I got it")
+router.get("/login", (req, res) => {
+     res.render("login");
+   });
+
+router.get("/logout", (req, res) => {
+     req.session.destroy((error) => {
+       if (error) {
+         console.log(error);
+       } else {
+         console.log("logout successfully");
+         res.redirect("/login");
+       }
+     });
+   });
+router.get("/", (req, res) => {
+     if (req.session.user) {
+       res.render("home");
+     } else {
+       res.redirect("/login");
+     }
+   });
+router.post("/form-submit", (req, res) => {
+     if (req.body.email === user.userName && req.body.password == user.password) {
+       req.session.user = req.body.email;
+       console.log("Logged In");
+       res.redirect("/");
+     } else {
+       res.render("login", { layout: "./layouts/invalid.ejs" });
+     }
    });
 
 module.exports = router
+
+// for app.js
