dd6c7009ca61d1554c4f0574176f07b847c3b0ee
Roshith <107235573+roshith844@users.noreply.github.com>
2022-11-13 06:17:30 +0000

    Change MongoDB connection to Atlas (MongoDB cloud)

--- db-connection.js
+++ db-connection.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 
+// Example for Localhost MongoDB connection Link :mongodb://localhost:27017/userData//
+
 mongoose
-  .connect("mongodb://localhost:27017/userData", {
+  .connect("mongodb+srv://roshith:7906@cluster0.qvn5gxo.mongodb.net/userData?retryWrites=true&w=majority", {
     // For not getting depreciation warning from mongoose
     //     useNewUrlParser: true,
     //     useUnifiedTopology: true,
